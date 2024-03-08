bffceb3ba3e53aa1d6056319735c587aaf831b8e
MaSalcedo10 <masg1606@hotmai.com>
2024-03-08 15:03:45 -0500

    Comments

--- primeNumber.java
+++ primeNumber.java
@@ -9,11 +9,12 @@ public class primeNumber {
         int number = num.nextInt();
         
         int addition = 0;
-
+//With a loop (for) we look for the residues of the division of the given number and it will help us know if it is equal to zero
         for(int i=1; i<=number; i++){
             
             int assistant = number%i;
-           
+//With a (if) we will count the residues to zero and if the residues are 2 it only means that it is a prime number but if it has 
+//more than two residues it means that it is not prime.           
             if( assistant==0){
                 addition = addition+1;
             }
